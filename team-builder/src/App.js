489d136aa5ca84360f3ff1fb6c5de2ed0afd0662
Jared Brown <jared@Jareds-MBP.lan1>
2019-12-10 21:04:31 -0800

    mvp complete

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -26,7 +26,7 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="App">
       <h1>Team Builder</h1>
       <NoteForm addNewNote={addNewNote} />
       <Notes notes={notes} />
--- team-builder/src/Components/NoteForm.js
+++ team-builder/src/Components/NoteForm.js
@@ -1,24 +1,31 @@
 import React, { useState } from "react";
 
 const NoteForm = ({ addNewNote }) => {
-  const [note, setNote] = useState({ id: "", title: "", body: "" });
+  const [note, setNote] = useState({ id: "", name: "", email: "", role: "" });
 
-  //   const handleChanges = event => {
-  //     setNote({ ...note, [event.target.value]: event.target.value });
-  //   };
+  const handleChanges = event => {
+    setNote({ ...note, [event.target.name]: event.target.value });
+  };
 
   const submitForm = event => {
     event.preventDefault();
     addNewNote(note);
 
-    setNote({ title: "", body: "" });
+    setNote({ name: "", email: "", role: "" });
   };
 
   return (
     <form onSubmit={submitForm}>
       <label htmlFor>Name</label>
 
-      <input id="name" type="text" name="name" placeholder="Enter your name" />
+      <input
+        id="name"
+        type="text"
+        name="name"
+        placeholder="Enter your name"
+        onChange={handleChanges}
+        //value={note.name}
+      />
 
       <label htmlFor="email">Email</label>
 
@@ -27,11 +34,20 @@ const NoteForm = ({ addNewNote }) => {
         type="email"
         name="email"
         placeholder="Enter your email"
+        onChange={handleChanges}
+        //value={note.email}
       />
 
       <label htmlFor="role">Role</label>
 
-      <input id="role" type="text" name="role" placeholder="Enter your role" />
+      <input
+        id="role"
+        type="text"
+        name="role"
+        placeholder="Enter your role"
+        onChange={handleChanges}
+        //value={note.role}
+      />
 
       <button type="submit">Add Team Member</button>
     </form>
